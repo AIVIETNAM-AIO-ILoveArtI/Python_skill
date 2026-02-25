8254aaf595535bac9b43234347a3ce1aa446a5cc
Nguyen Hai Duong Anh <ldrawinglifel@gmail.com>
2026-02-25 13:21:39 +0700

    Create script to print coprime numbers with n
    Add a script to find numbers coprime with n.

---
+++ PTIT/NGUYÊN TỐ CÙNG NHAU.py
@@ -0,0 +1,9 @@
+import math
+n,k = map(int,input().split())
+dem =0
+for i in range(10**(k-1),10**k):
+    if math.gcd(i,n)==1:
+        print(i,end=' ')
+        dem += 1
+        if dem%10==0 and dem!=0:
+            print()
